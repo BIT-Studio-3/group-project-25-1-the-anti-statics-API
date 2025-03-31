46f7af3208b85c31b831704d42b7098ddfdcc34c
SamBat2005 <batcsg1@student.op.ac.nz>
2025-04-01 08:55:51 +1300

    refactor: reordered app.js content

--- app.js
+++ app.js
@@ -9,26 +9,11 @@ import indexRoutes from "./routes/index.js";
 //Import the cors module
 import cors from 'cors';
 
-// Create an Express application
-const app = express();
-
-// Use the PORT environment variable or 3000
-const PORT = process.env.PORT || 3000;
-
-// Use the routes module
-app.use("/", indexRoutes);
-
-// Start the server on port 3000
-app.listen(PORT, () => {
-  console.log(
-    `Server is listening on port ${PORT}. Visit http://localhost:${PORT}`
-  );
-});
-
 // Import the alerts routes module
 import alertRoutes from "./routes/v1/alerts.js";
-import damageRoutes from "./routes/v1/damages.js";
 
+// Import the damage routes module
+import damageRoutes from "./routes/v1/damages.js";
 
 // Import the hazards routes module
 import hazardRoutes from "./routes/v1/hazards.js";
@@ -38,18 +23,19 @@ import resourceRoutes from "./routes/v1/ResourcesAvailability.js";
 
 import logger from "./middleware/logger.js";
 
-app.use((req, res, next) => {
-  logger.info(`${req.method} ${req.originalUrl}`);
-  next();
-});
-
 import auth from "./middleware/auth.js";
 
 import authRoutes from "./routes/v1/auth.js";
 
 import { isContentTypeApplicationJSON } from "./middleware/utils.js";
 
-app.use("/api/v1/auth", authRoutes);
+// Create an Express application
+const app = express();
+
+// Use the PORT environment variable or 3000
+const PORT = process.env.PORT || 3000;
+
+// Use the routes module
 
 //Use the CORS module
 //This will allow request from any origin
@@ -85,20 +71,34 @@ const swaggerOptions = {
 
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
+app.use("/", indexRoutes);
+app.use("/api/v1/auth", authRoutes);
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Use the alerts route
 app.use("/api/v1/alerts", alertRoutes);
-app.use("/api/v1/damages", damageRoutes);
 
+//Use the damages route
+app.use("/api/v1/damages", damageRoutes);
 
 // Use the hazards route
 app.use("/api/v1/hazards", hazardRoutes);
 
-// Use the hazards route
+// Use the resources route
 app.use("/api/v1/ResourcesAvailability", resourceRoutes);
 
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.originalUrl}`);
+  next();
+});
 //Test commit 28/3/2025
 
+// Start the server on port 3000
+app.listen(PORT, () => {
+  console.log(
+    `Server is listening on port ${PORT}. Visit http://localhost:${PORT}`
+  );
+});
 // Export the Express application. May be used by other modules. For example, API testing
 export default app;
