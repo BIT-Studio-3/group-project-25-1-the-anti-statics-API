aefa01331808b7a90256db90baa8109b2ecbb09f
Hazel <mapuhn1@student.op.ac.nz>
2025-05-01 22:44:28 +1200

    build: define post and put validation in resources availability route file

--- routes/v1/ResourcesAvailability.js
+++ routes/v1/ResourcesAvailability.js
@@ -13,6 +13,11 @@ import {
   deleteResourceAvailability,
 } from "../../controllers/v1/ResourcesAvailability.js";
 
+import {
+  validatePostResources,
+  validatePutResources
+} from "../../middleware/validation/ResourcesAvailability.js"
+
 const resourceController = {
     get: getResourcesAvailability,
     getById: getResourceAvailability,
@@ -21,6 +26,10 @@ const resourceController = {
     delete: deleteResourceAvailability,
   };
 
-const resourceRouter = createRouter(resourceController);
+const resourceRouter = createRouter(
+  resourceController,
+  validatePostResources,
+  validatePutResources
+);
 
 export default resourceRouter;
